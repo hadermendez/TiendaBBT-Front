d9ddb3ad9218e897bd75555da4ecbc5fc64cd39c
Hader Mendez <hrmendezg@gmail.com>
2025-07-11 17:12:03 -0500

    se ajusta listado de productos en 2 columnas

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,68 +1,76 @@
-import { formatMoney } from "../helpers"
-import useTienda from "../hooks/useTienda"
+import { formatMoney } from "../helpers";
+import useTienda from "../hooks/useTienda";
 
-export default function Producto({producto, botonAgregar = false, botonDisponible = false}) {
-    const { handleClickModal, handleSetProdcuto,handleClickProductoAgotado} = useTienda()
+export default function Producto({
+  producto,
+  botonAgregar = false,
+  botonDisponible = false,
+}) {
+  const { handleClickModal, handleSetProdcuto, handleClickProductoAgotado } =
+    useTienda();
 
-    const {nombre, imagen, precio} = producto
-        // Obtiene la URL base de la API desde las variables de entorno
-    const apiUrlBase = import.meta.env.VITE_API_URL;
+  const { nombre, imagen, precio } = producto;
+  // Obtiene la URL base de la API desde las variables de entorno
+  const apiUrlBase = import.meta.env.VITE_API_URL;
 
-    // Remueve el prefijo 'public/' de la ruta de la imagen y ajusta la ruta para apuntar a 'storage/imagenes/'
-    const imagePath = imagen.replace('public/', 'storage/');
+  // Remueve el prefijo 'public/' de la ruta de la imagen y ajusta la ruta para apuntar a 'storage/imagenes/'
+  const imagePath = imagen.replace("public/", "storage/");
 
-    // Construye la URL completa de la imagen
-    const imageUrl = new URL(imagePath, apiUrlBase).href;
-    console.log(imageUrl)
+  // Construye la URL completa de la imagen
+  const imageUrl = new URL(imagePath, apiUrlBase).href;
+  console.log(imageUrl);
   return (
-    <div className=" border p-3 shadow bg-white">
-        <img 
-            src={imageUrl} 
-            alt={`imagen ${nombre}`}
-            className="w-full" 
-        />
+    // <div className=" border p-3 shadow bg-white">
+    <div className="border p-3 shadow bg-white rounded-lg flex flex-col h-full">
+      <img
+        src={imageUrl}
+        alt={`imagen ${nombre}`}
+        className="w-full h-40 object-cover rounded-md"
+      />
 
-        <div className="p-5">
-            <h3 className="text-2xl font-bold">{nombre}</h3>
-            <p className=" mt-5 font-black text-4xl text-indigo-500">
-                {formatMoney(precio)}
-            </p>
-            {botonAgregar && (
+      <div className="p-5">
+        <h3 className="text-2xl font-bold">{nombre}</h3>
+        <p className="mt-4 font-extrabold text-indigo-500 text-lg sm:text-xl md:text-2xl whitespace-nowrap overflow-hidden text-center">
+          <span className="align-middle">$</span>{" "}
+          {formatMoney(precio).replace("$", "")}
+        </p>
 
-                <button
-                    type="button"
-                    className=" bg-black hover:bg-gray-400 text-white w-full mt-5 p-3 uppercase font-bold"
-                    onClick={()=>{
-                        handleClickModal();
-                        handleSetProdcuto(producto);
-                    }}    
-                    >
-                    Agregar
-                </button>
-            )}
-            {botonDisponible ? (
-                <button
-                    type="button"
-                    className=" bg-black hover:bg-gray-400 text-white w-full mt-5 p-3 uppercase font-bold"
-                    onClick={()=>{
-                        handleClickProductoAgotado(producto.id);
-                    }}    
-                    >
-                    Producto Agotado
-                </button>
-            ):!(botonAgregar) && (
-                <button
-                    type="button"
-                    className=" bg-black hover:bg-gray-400 text-white w-full mt-5 p-3 uppercase font-bold"
-                    onClick={()=>{
-                        handleClickProductoAgotado(producto.id);
-                    }}    
-                    >
-                    Producto Disponible
-                </button>
-            )}
-            
-        </div>
+        {botonAgregar && (
+          <button
+            type="button"
+            className="mt-auto bg-black hover:bg-gray-700 text-white w-full py-2 mt-4 rounded font-semibold uppercase tracking-wide text-sm"
+            onClick={() => {
+              handleClickModal();
+              handleSetProdcuto(producto);
+            }}
+          >
+            Agregar
+          </button>
+        )}
+        {botonDisponible ? (
+          <button
+            type="button"
+            className=" bg-black hover:bg-gray-400 text-white w-full mt-5 p-3 uppercase font-bold"
+            onClick={() => {
+              handleClickProductoAgotado(producto.id);
+            }}
+          >
+            Producto Agotado
+          </button>
+        ) : (
+          !botonAgregar && (
+            <button
+              type="button"
+              className=" bg-black hover:bg-gray-400 text-white w-full mt-5 p-3 uppercase font-bold"
+              onClick={() => {
+                handleClickProductoAgotado(producto.id);
+              }}
+            >
+              Producto Disponible
+            </button>
+          )
+        )}
+      </div>
     </div>
-  )
+  );
 }
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,57 +1,57 @@
-
-import useSWR from'swr'
+import useSWR from "swr";
 import Producto from "../components/Producto";
 import clienteAxios from "../config/axios";
 import { productos as data } from "../data/productos";
 import useTienda from "../hooks/useTienda";
 
-import ErrorBoundary from '../components/ErrorBoundary'; 
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export default function Inicio() {
+  const { categoriaActual } = useTienda();
+  const token = localStorage.getItem("AUTH_TOKEN");
 
-  const {categoriaActual} = useTienda()
-  const token = localStorage.getItem('AUTH_TOKEN')
-
-  const fetcher = () => clienteAxios('api/productos',{
-    headers:{
-      Authorization: `Bearer ${token}`
-    }
-  }).then(data => data.data)
-  const {data, error, isLoading} = useSWR('api/productos',fetcher,{
+  const fetcher = () =>
+    clienteAxios("api/productos", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((data) => data.data);
+  const { data, error, isLoading } = useSWR("api/productos", fetcher, {
     refreshInterval: 1000,
-  })
-  console.log(data)
-  console.log(error)
+  });
+  console.log(data);
+  console.log(error);
   // console.log(isloading)
-//   return
-  if(isLoading) return <div>Cargando...</div>;
+  //   return
+  if (isLoading) return <div>Cargando...</div>;
   if (error) return <div>Error al cargar</div>;
   if (!data) return <div>Cargando...</div>;
-  const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id)
+  const productos = data.data.filter(
+    (producto) => producto.categoria_id === categoriaActual.id
+  );
 
   return (
     // <>
     <ErrorBoundary>
       <div>
+        <h1 className="text-4xl font-black">
+          {categoriaActual ? categoriaActual.nombre : "Cargando categoría..."}
+        </h1>
 
-      <h1 className="text-4xl font-black">
-          {categoriaActual ? categoriaActual.nombre : 'Cargando categoría...'}
-      </h1>
-
-      <p className=" text-2xl my-10">
-        Elige tu pedido.
-      </p>
-      <div className=" grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-          {
-            productos?.filter(producto => producto.disponible).map(producto => (
-            < Producto 
-              key={producto.imagen}
-              producto={producto}
-              botonAgregar={true}
-            />
-          ))}
-      </div>
+        <p className=" text-2xl my-10">Elige tu pedido.</p>
+        <div className=" grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+          {/* <div className=" grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3"> */}
+          {productos
+            ?.filter((producto) => producto.disponible)
+            .map((producto) => (
+              <Producto
+                key={producto.imagen}
+                producto={producto}
+                botonAgregar={true}
+              />
+            ))}
+        </div>
       </div>
     </ErrorBoundary>
-  )
+  );
 }
